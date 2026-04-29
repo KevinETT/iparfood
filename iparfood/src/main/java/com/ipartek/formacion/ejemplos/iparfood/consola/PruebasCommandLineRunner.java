a58ce7748dd04217ad43669031bb4828be0ac8d6
Kevett <Kevett@KEVINPC>
2026-04-29 21:10:08 +0200

    Quitar el comand line

--- iparfood/src/main/java/com/ipartek/formacion/ejemplos/iparfood/consola/PruebasCommandLineRunner.java
+++ iparfood/src/main/java/com/ipartek/formacion/ejemplos/iparfood/consola/PruebasCommandLineRunner.java
@@ -23,7 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 
-@Component
+//@Component
 public class PruebasCommandLineRunner implements CommandLineRunner {
 	private final AdministradorService administradorService;
 	private final UsuarioService usuarioService;
